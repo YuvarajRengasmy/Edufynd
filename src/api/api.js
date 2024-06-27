ed0e2028d3abf2194e3f2614e387bdb0570b31f2
yuvarajud <119421277+YuvarajRengasmy@users.noreply.github.com>
2024-06-27 10:37:22 +0530

    inite

--- src/api/api.js
+++ src/api/api.js
@@ -1,49 +1,31 @@
 import axios from 'axios';
- import { clearStorage } from '../Utils/storage';
+import { clearStorage } from '../Utils/storage';
 
-// Create Axios instance with base URL
 const API = axios.create({ baseURL: 'https://api.edufynd.in/api/' });
 
-// Request interceptor
-// API.interceptors.request.use(
-//     config => {
-//         // Retrieve token from localStorage
-//         const token = localStorage.getItem("token");
-        
-//         // Set basic authentication header (if needed)
-//         const basicAuth = `Basic ` + btoa("edufynd:DAF87DSFDSFDSA98FSADKJE324KJL32HFD7FDSFB24343J49DSF");
-//         config.headers.authorization = basicAuth;
-        
-//         // Attach token to request headers
-//         if (token) {
-//             config.headers.token = `Bearer ${token}`;
-//         }
-        
-//         return config;
-//     },
-//     error => {
-//         // Handle request errors
-//         return Promise.reject(error);
-//     }
-// );
 
-// Response interceptor
-API.interceptors.response.use(
-    response => {
-        // Handle successful responses
-        return response;
-    },
-    error => {
-        // Handle error responses
+API.interceptors.request.use(request => {
+    const token = localStorage.getItem("token")
+    const basicAuth = `Basic ` + btoa("edufynd:DAF87DSFDSFDSA98FSADKJE324KJL32HFD7FDSFB24343J49DSF");
+    request.headers.authorization = basicAuth;
+    if (token) {
+        request.headers.token = `Bearer ${token}`;
+    }
+    return request
+}, (error) => {
+    return Promise.reject(error);
+}
+)
+
+API.interceptors.response.use(response => {
+    return response
+},
+    (error) => {
         if (error.response && error.response.status === 401) {
-            // Clear storage and redirect if unauthorized
-            clearStorage();
+            clearStorage()
             window.location.pathname = '/';
         }
         return Promise.reject(error);
-    }
-);
+    })
 
 export default API;
-
-
