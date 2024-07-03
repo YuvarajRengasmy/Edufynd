9c6664199dfca4a60795f45e6ff4ea65cb1c96e3
JayJo17 <gopinath172023@gmail.com>
2024-07-03 20:02:57 +0530

    general and business

--- src/SuperAdmin/Enquiry/General/editGeneralEnquiry.js
+++ src/SuperAdmin/Enquiry/General/editGeneralEnquiry.js
@@ -10,50 +10,50 @@ export const editGeneralEnquiry = () => {
         <div className='content-wrapper' style={{fontSize:'14px' }}>
 <div className='content-header'>
   <div className='container card card-body p-4 border-0'>
-    <h4 className='card-title text-center fw-bold'>General Enquiry</h4>
+    <h4 className='card-title text-center fw-bold'>Edit General Enquiry</h4>
     <hr/>
   <form className="p-1">
    
     <div className='row mb-3'>
     <div className="col">
                           <label className="form-label" for="inputEmail4">Student Name</label>
-                          <input className="form-control" id="inputEmail4"  type="text" placeholder='Enter Name'/>
+                          <input className="form-control" id="inputEmail4"  type="text" placeholder='Enter Name' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputPassword4">DOB</label>
-                          <input className="form-control" id="inputPassword4" type="text" placeholder='DOB' />
+                          <input className="form-control" id="inputPassword4" type="text" placeholder='DOB' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }} />
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputAddress">Passport No</label>
-                          <input className="form-control" id="inputAddress" type="text" placeholder="Passport No"/>
+                          <input className="form-control" id="inputAddress" type="text" placeholder="Passport No" style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
     </div>
     <div className='row mb-3'>
     <div className="col">
                           <label className="form-label" for="inputEmail4">Contact Number</label>
-                          <input className="form-control" id="inputEmail4" type="text" placeholder='Contact Number'/>
+                          <input className="form-control" id="inputEmail4" type="text" placeholder='Contact Number' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputPassword4">Email ID</label>
-                          <input className="form-control" id="inputPassword4" type="Password" placeholder='Email ID' />
+                          <input className="form-control" id="inputPassword4" type="Password" placeholder='Email ID' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }} />
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputAddress">CGPA / Year passed</label>
-                          <input className="form-control" id="inputAddress" type="text" placeholder="CGPA / Year passed"/>
+                          <input className="form-control" id="inputAddress" type="text" placeholder="CGPA / Year passed" style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
     </div>
     <div className='row mb-3'>
     <div className="col">
                           <label className="form-label" for="inputEmail4">Desired Country</label>
-                          <input className="form-control" id="inputEmail4" type="text" placeholder='Desired Country'/>
+                          <input className="form-control" id="inputEmail4" type="text" placeholder='Desired Country' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputPassword4">Desired Course</label>
-                          <input className="form-control" id="inputPassword4" type="Password" placeholder='Desired Course' />
+                          <input className="form-control" id="inputPassword4" type="Password" placeholder='Desired Course' style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }} />
                         </div>
                         <div className="col">
                           <label className="form-label" for="inputAddress">Do you need support for loan? </label>
-                          <input className="form-control" id="inputAddress" type="text" placeholder="Do you need support for loan? "/>
+                          <input className="form-control" id="inputAddress" type="text" placeholder="Do you need support for loan? " style={{fontFamily: 'Plus Jakarta Sans', fontSize:'12px' }}/>
                         </div>
     </div>
                     <div className='row mb-3'>
