761fcd8376c3a641542742df3bb94c258537cd67
Yuvaraj R <yuvarajr998@gmail.com>
2024-07-11 11:18:13 -0700

    in

--- src/SuperAdmin/University/Country.js
+++ src/SuperAdmin/University/Country.js
@@ -1,82 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import universities from './universities';
 
-const YourComponent = () => {
-  const [campuses, setCampuses] = useState([]);
+const UniversitySelector = () => {
+  const [country, setCountry] = useState('');
+  const [filteredUniversities, setFilteredUniversities] = useState([]);
+  
+  const countries = [...new Set(universities.map(uni => uni.country))];
 
-  const addCampus = () => {
-    const newCampus = {
-      intake: '',
-      courseFee: '',
-      duration: ''
-    };
-    setCampuses([...campuses, newCampus]);
+  useEffect(() => {
+    if (country) {
+      const filtered = universities.filter(uni => uni.country === country);
+      setFilteredUniversities(filtered);
+    } else {
+      setFilteredUniversities([]);
+    }
+  }, [country]);
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value);
   };
 
-  const handleInputChange = (index, fieldName, value) => {
-    const updatedCampuses = [...campuses];
-    updatedCampuses[index][fieldName] = value;
-    setCampuses(updatedCampuses);
+  const handleSubmit = () => {
+    console.log('Selected country code:', country);
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-4 col-md-6 col-sm-6">
-          <div>
-            <button
-              type="button"
-              onClick={addCampus}
-              style={{ marginTop: '1.8rem', backgroundColor: '#9265cc' }}
-              className="btn text-white"
-            >
-              Add Campus
-            </button>
-          </div>
-        </div>
-
-        {campuses.map((campus, index) => (
-          <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
-            <div style={{ marginTop: '1rem' }}>
-              <label>Campus:</label>
-              <input
-                type="text"
-                value={campus.campus}
-                onChange={(e) => handleInputChange(index, 'intake', e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div style={{ marginTop: '1rem' }}>
-              <label>Intake:</label>
-              <input
-                type="text"
-                value={campus.intake}
-                onChange={(e) => handleInputChange(index, 'intake', e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div style={{ marginTop: '1rem' }}>
-              <label>Course Fee:</label>
-              <input
-                type="text"
-                value={campus.courseFee}
-                onChange={(e) => handleInputChange(index, 'courseFee', e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div style={{ marginTop: '1rem' }}>
-              <label>Duration:</label>
-              <input
-                type="text"
-                value={campus.duration}
-                onChange={(e) => handleInputChange(index, 'duration', e.target.value)}
-                className="form-control"
-              />
-            </div>
-          </div>
+    <div>
+      <label>Select Country</label>
+      <select onChange={handleCountryChange} value={country}>
+        <option value="">Select a country</option>
+        {countries.map((c, index) => (
+          <option key={index} value={c}>{c}</option>
         ))}
-      </div>
+      </select>
+
+      <label>Universities</label>
+      <ul>
+        {filteredUniversities.map((uni, index) => (
+          <li key={index}>{uni.name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
 
-export default YourComponent;
+export default UniversitySelector;
