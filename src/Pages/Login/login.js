4a92c766140eed991d6e63821fdcb03a44bbffa5
JayJo17 <gopinath172023@gmail.com>
2024-07-24 12:33:19 +0530

    login and register  Ui Updated

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -104,21 +104,22 @@ if (isAuthenticated()) {
   return (
     <>
       <div style={{ backgroundColor: '#f5f5f5', fontFamily: 'Plus Jakarta Sans', fontSize: '14px' }}>
-        <div className="container-fluid p-3">
-          <div className="row justify-content-center align-items-center">
+        <div className="container-fluid p-4">
+          <div className="row  justify-content-center align-items-center">
             <div className="col-xl-4 col-lg-4 col-md-7 align-self-center">
-              <div className="card card-body border-0 p-5 my-5 ">
+              <div className="card border-0 my-5  ">
                 
-               
-                  
-                   
-                      
-                        <div className="text-center ">
+               <div className="card-body p-4">
+               <div className="text-center my-2 ">
                           <h1 className="h3  fw-semibold">Welcome Back!</h1>
                         </div>
+
+
+
                         <form className="user" onSubmit={handleSubmit}>
-                          <div className="form-group">
-                            <input type="email" name="email" onChange={handleInputs} className="form-control  rounded-2" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address..." style={{fontSize:'12px'}} />
+                        <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1"><i class="fa fa-user nav-icon "></i></span>
+                            <input type="email" name="email" onChange={handleInputs} className="form-control  " id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address..." style={{fontSize:'12px'}} />
                             {errors.email.required ? (
                               <div className="text-danger form-text">
                                 This field is required.
@@ -129,8 +130,9 @@ if (isAuthenticated()) {
                               </div>
                             ) : null}
                           </div>
-                          <div className="form-group">
-                            <input type="password" name="password" onChange={handleInputs} autoComplete="off" className="form-control     rounded-2" id="exampleInputPassword" placeholder="Password..." style={{fontSize:'12px'}} />
+                          <div class="input-group mb-3">
+                          <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock nav-icon"></i></span>
+                            <input type="password" name="password" onChange={handleInputs} autoComplete="off" className="form-control     " id="exampleInputPassword" placeholder="Password..." style={{fontSize:'12px'}} />
                             {errors.password.required ? (
                               <div className="text-danger form-text">
                                 This field is required.
@@ -152,7 +154,7 @@ if (isAuthenticated()) {
                             </div>
                           </div>
                           <div className='d-flex justify-content-center'>
-                            <button type="submit" className="w-75 btn btn-sm   border-0  fw-bold text-uppercase px-4 py-2" style={{ backgroundColor: '#fe5722',color:'#fff',fontSize:'10px' }}>Login </button>
+                            <button type="submit" className="w-75 btn btn-sm   border-0  fw-bold text-uppercase px-4 py-2" style={{ backgroundColor: '#fe5722',color:'#fff',fontSize:'10px' }}><i class="fa fa-sign-in-alt nav-icon text-white"></i>Login </button>
                           </div>
       
                           <div class="alternative-login d-flex flex-row align-items-center justify-content-center" style={{marginTop:'1rem',marginBottom:'1rem'}}>
@@ -165,16 +167,22 @@ if (isAuthenticated()) {
                             <i className="fab fa-google fa-fw" /> Login with Google
                           </Link>
                           </div>
-                        </form>
-                       
-                         <div className="row g-3 text-center my-2">
+                          <div className="row g-3 text-center my-2">
                          <div className="col-md-6 col-sm-12">
-                          <Link to='/ForgotPassword' className="btn btn-sm   border-0 text-uppercase  fw-bold px-4 py-2" style={{backgroundColor:'#f0f0f0',color:'#231f20',fontSize:'9px'}} >Forgot Password</Link>
+                          <Link to='/ForgotPassword' className="btn btn-sm   border-0 text-uppercase  fw-bold px-4 py-2" style={{backgroundColor:'#f0f0f0',color:'#231f20',fontSize:'9px'}} ><i class="fa fa-key nav-icon"></i>Forgot Password</Link>
                         </div>
                         <div className="col-md-6 col-sm-12">
-                          <Link to='/Register' className="btn btn-sm  px-4 text-uppercase fw-bold py-2" style={{backgroundColor:'#34A853',color:'#fff',fontSize:'9px'}}>Create Account!</Link>
+                          <Link to='/Register' className="btn btn-sm  px-4 text-uppercase fw-bold py-2" style={{backgroundColor:'#34A853',color:'#fff',fontSize:'9px'}}><i class="fa fa-plus-circle nav-icon text-white"></i>Create Account!</Link>
                         </div>
                          </div>
+                        </form>
+                       
+                       
+               </div>
+                  
+                   
+                      
+                       
                      
                       
                     
