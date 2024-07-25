5a851b53dca079bbfa07598403de401b0bbb9cce
JayJo17 <gopinath172023@gmail.com>
2024-07-25 10:25:18 +0530

    page reload

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -46,157 +46,139 @@ const Login = () => {
         }
     }
     return true;
-}
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newError = handleValidation(inputs)
-    setErrors(newError)
-    setSubmitted(true)
+    const newError = handleValidation(inputs);
+    setErrors(newError);
+    setSubmitted(true);
     if (handleErrors(newError)) {
         loginUser(inputs).then(res => {
             let token = res?.data?.result?.token;
-            let loginType = res?.data?.result?.loginType
+            let loginType = res?.data?.result?.loginType;
             if (loginType === 'student') {
                 let studentId = res?.data?.result?.studentDetails?._id;
                 let data = {
-                    token: token, studentId:studentId , loginType: loginType
-                }
+                    token: token, studentId: studentId, loginType: loginType
+                };
                 saveToken(data);
                 if (isAuthenticated()) {
                     navigate("/Student");
+                    window.location.reload(); // Refresh the page
                 }
             }
             if (loginType === 'superAdmin') {
                 let superAdminId = res?.data?.result?.superAdminDetails?._id;
                 let data = {
                     token: token, superAdminId: superAdminId, loginType: loginType
-                }
+                };
                 saveToken(data);
                 if (isAuthenticated()) {
                     navigate("/Dashboard");
+                    window.location.reload(); // Refresh the page
                 }
             }
             if (loginType === 'agent') {
                 let agentId = res?.data?.result?.agentDetails?._id;
                 let data = {
                     token: token, agentId: agentId, loginType: loginType
-                }
+                };
                 saveToken(data);
                 if (isAuthenticated()) {
                     navigate("/AgentHome");
+                    window.location.reload(); // Refresh the page
                 }
             }
             toast.success(res?.data?.message);
         })
-            .catch((err) => {
-                toast.error(err?.response?.data?.message);
-            });
+        .catch((err) => {
+            toast.error(err?.response?.data?.message);
+        });
     }
-}
+  }
 
-if (isAuthenticated()) {
-    const type = getLoginType()
+  if (isAuthenticated()) {
+    const type = getLoginType();
     if (type === 'student') { return <Navigate to="/Student" /> }
     else if (type === 'superAdmin') { return <Navigate to="/Dashboard" /> }
     else { return <Navigate to="/AgentHome" /> }
-}
+  }
+
   return (
-    <>
-      <div style={{ backgroundColor: '#f5f5f5', fontFamily: 'Plus Jakarta Sans', fontSize: '14px' }}>
-        <div className="container-fluid p-4">
-          <div className="row  justify-content-center align-items-center">
-            <div className="col-xl-4 col-lg-4 col-md-7 align-self-center">
-              <div className="card border-0 my-5  ">
-                
-               <div className="card-body p-4">
-               <div className="text-center my-2 ">
-                          <h1 className="h3  fw-bold"style={{color:'#E91E63'}}>Member Login!</h1>
-                        </div>
-
-
-
-                        <form className="user" onSubmit={handleSubmit}>
-                        <div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1"><i class="fa fa-user nav-icon "></i></span>
-                            <input type="email" name="email" onChange={handleInputs} className="form-control  " id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address..." style={{fontSize:'12px'}} />
-                          
-                          
-                          </div>
-                          {errors.email.required ? (
-                              <div className="text-danger form-text">
-                                This field is required.
-                              </div>
-                            ) : errors.email.valid ? (
-                              <div className="text-danger form-text">
-                                Enter valid Email Id.
-                              </div>
-                            ) : null}
-                          <div class="input-group mb-3">
-                          <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock nav-icon"></i></span>
-                            <input type="password" name="password" onChange={handleInputs} autoComplete="off" className="form-control     " id="exampleInputPassword" placeholder="Password..." style={{fontSize:'12px'}} />
-                           
-                          </div>
-                          {errors.password.required ? (
-                              <div className="text-danger form-text">
-                                This field is required.
-                              </div>
-                            ) : errors.password.valid ? (
-                              <div className="text-danger form-text">
-                                A minimum 8 characters password contains a <br />
-                                combination of {''}
-                                <strong>uppercase, lowercase, {''}</strong>
-                                <strong>special <br /> character{''}</strong> and <strong>number</strong>.
-                              </div>
-                            ) : null}
-                          <div className="form-group">
-                            <div className="custom-control custom-checkbox small">
-                              <input type="checkbox" className="form-check-input" id="customCheck" />
-                              <label className="form-check-label fw-semibold" htmlFor="customCheck">Remember
-                                Me</label>
-                            </div>
-                          </div>
-                          <div className='d-flex justify-content-center'>
-                            <button type="submit" target='_self' className="w-75 btn btn-sm   border-0  fw-bold text-uppercase px-4 py-2" style={{ backgroundColor: '#fe5722',color:'#fff',fontSize:'10px' }}><i class="fa fa-sign-in-alt nav-icon text-white"></i>Login </button>
-                          </div>
-      
-                          <div class="alternative-login d-flex flex-row align-items-center justify-content-center" style={{marginTop:'1rem',marginBottom:'1rem'}}>
-                <hr className="border-0 border-top border-dark" style={{margin:'0 1rem',flexGrow:'1',borderTop:'1px solid #ccc'}}/>
-                <span>or</span>
-                <hr className="border-0 border-top border-dark " style={{margin:'0 1rem',flexGrow:'1',borderTop:'1px solid #ccc'}}/>
-              </div>
-              <div className='d-flex justify-content-center'>
-                          <Link to='/'  className="btn btn-google btn-user btn-block  btn-sm   border-0  fw-bold text-uppercase  px-4 py-2 w-75" style={{backgroundColor:'#4285F4',color:'#fff',fontSize:'10px'}}>
-                            <i className="fab fa-google fa-fw" /> Login with Google
-                          </Link>
-                          </div>
-                          <div className="row g-3 text-center my-2">
-                         <div className="col-md-6 col-sm-12">
-                          <Link to='/ForgotPassword' target="_self" className="btn btn-sm   border-0 text-uppercase  fw-bold px-4 py-2" style={{backgroundColor:'#f0f0f0',color:'#231f20',fontSize:'9px'}} ><i class="fa fa-key nav-icon"></i>Forgot Password</Link>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                          <Link to='/Register' target="_self" className="btn btn-sm  px-4 text-uppercase fw-bold py-2" style={{backgroundColor:'#34A853',color:'#fff',fontSize:'9px'}}><i class="fa fa-plus-circle nav-icon text-white"></i>Create Account!</Link>
-                        </div>
-                         </div>
-                        </form>
-                       
-                       
-               </div>
-                  
-                   
-                      
-                       
-                     
-                      
-                    
-                 
-                
+    <div style={{ backgroundColor: '#f5f5f5', fontFamily: 'Plus Jakarta Sans', fontSize: '14px' }}>
+      <div className="container-fluid p-4">
+        <div className="row  justify-content-center align-items-center">
+          <div className="col-xl-4 col-lg-4 col-md-7 align-self-center">
+            <div className="card border-0 my-5">
+              <div className="card-body p-4">
+                <div className="text-center my-2">
+                  <h1 className="h3 fw-bold" style={{ color: '#E91E63' }}>Member Login!</h1>
+                </div>
+                <form className="user" onSubmit={handleSubmit}>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1"><i className="fa fa-user nav-icon"></i></span>
+                    <input type="email" name="email" onChange={handleInputs} className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address..." style={{ fontSize: '12px' }} />
+                  </div>
+                  {errors.email.required ? (
+                    <div className="text-danger form-text">
+                      This field is required.
+                    </div>
+                  ) : errors.email.valid ? (
+                    <div className="text-danger form-text">
+                      Enter valid Email Id.
+                    </div>
+                  ) : null}
+                  <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1"><i className="fa fa-lock nav-icon"></i></span>
+                    <input type="password" name="password" onChange={handleInputs} autoComplete="off" className="form-control" id="exampleInputPassword" placeholder="Password..." style={{ fontSize: '12px' }} />
+                  </div>
+                  {errors.password.required ? (
+                    <div className="text-danger form-text">
+                      This field is required.
+                    </div>
+                  ) : errors.password.valid ? (
+                    <div className="text-danger form-text">
+                      A minimum 8 characters password contains a <br />
+                      combination of {''}
+                      <strong>uppercase, lowercase, {''}</strong>
+                      <strong>special <br /> character{''}</strong> and <strong>number</strong>.
+                    </div>
+                  ) : null}
+                  <div className="form-group">
+                    <div className="custom-control custom-checkbox small">
+                      <input type="checkbox" className="form-check-input" id="customCheck" />
+                      <label className="form-check-label fw-semibold" htmlFor="customCheck">Remember Me</label>
+                    </div>
+                  </div>
+                  <div className='d-flex justify-content-center'>
+                    <button type="submit" target='_self' className="w-75 btn btn-sm border-0 fw-bold text-uppercase px-4 py-2" style={{ backgroundColor: '#fe5722', color: '#fff', fontSize: '10px' }}><i className="fa fa-sign-in-alt nav-icon text-white"></i>Login</button>
+                  </div>
+                  <div className="alternative-login d-flex flex-row align-items-center justify-content-center" style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+                    <hr className="border-0 border-top border-dark" style={{ margin: '0 1rem', flexGrow: '1', borderTop: '1px solid #ccc' }} />
+                    <span>or</span>
+                    <hr className="border-0 border-top border-dark" style={{ margin: '0 1rem', flexGrow: '1', borderTop: '1px solid #ccc' }} />
+                  </div>
+                  <div className='d-flex justify-content-center'>
+                    <Link to='/' className="btn btn-google btn-user btn-block btn-sm border-0 fw-bold text-uppercase px-4 py-2 w-75" style={{ backgroundColor: '#4285F4', color: '#fff', fontSize: '10px' }}>
+                      <i className="fab fa-google fa-fw" /> Login with Google
+                    </Link>
+                  </div>
+                  <div className="row g-3 text-center my-2">
+                    <div className="col-md-6 col-sm-12">
+                      <Link to='/ForgotPassword' target="_self" className="btn btn-sm border-0 text-uppercase fw-bold px-4 py-2" style={{ backgroundColor: '#f0f0f0', color: '#231f20', fontSize: '9px' }}><i className="fa fa-key nav-icon"></i>Forgot Password</Link>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                      <Link to='/Register' target="_self" className="btn btn-sm px-4 text-uppercase fw-bold py-2" style={{ backgroundColor: '#34A853', color: '#fff', fontSize: '9px' }}><i className="fa fa-plus-circle nav-icon text-white"></i>Create Account!</Link>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
